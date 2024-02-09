f7f0749fdd5ca315ead6cc61d7b64804cff32e08
Kashew Knutt <kashewknutt@gmail.com>
2024-02-09 21:21:04 +0530

    Commit5

--- functionOfFunction/main.py
+++ functionOfFunction/main.py
@@ -1,10 +1,12 @@
 from source import func
 
 try_fun = func
+fun = func()
 print("Calling func:")
 func()
 print("Calling try_fun:")
 try_fun()
 print("Printing try_fun and calling simultaneously:", try_fun())
 print("Printing try_fun without calling:", try_fun)
+
 
